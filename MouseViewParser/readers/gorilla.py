4b7ff21984dfe50e1547efa82e51a97b2faa6f20
Edwin Dalmaijer <edwin.dalmaijer@gmail.com>
2021-08-25 16:09:48 +0100

    Added code for the new Gorilla mouse_view zone, and renamed old parser to gorilla_prezone

--- MouseViewParser/readers/gorilla.py
+++ MouseViewParser/readers/gorilla.py
@@ -7,8 +7,11 @@ import copy
 
 import numpy
 
+import openpyxl
 
-def read_file(file_path, trial_start_zone, custom_fields=None, delimiter=None):
+
+def read_file(file_path, trial_folder_path, custom_fields=None, \
+    use_public_id=False, verbose=False):
     
     # Check whether the file exists.
     if not os.path.isfile(file_path):
@@ -24,30 +27,15 @@ def read_file(file_path, trial_start_zone, custom_fields=None, delimiter=None):
             " as a delimiter-separated file. Its extension is" + \
             " {}, but '.csv', '.tsv', or '.txt' was expected.".format(ext))
     
-    # Set the delimiter.
-    if (delimiter is None) or (delimiter == "auto"):
-        if ext == ".csv":
-            delimiter = ","
-        elif ext == ".tsv":
-            delimiter = "\t"
-    
     # Open the file.
     with open(file_path, "r") as f:
         # Snif out the dialect. We're using a large chunk of data for this,
         # as the header gets quite big.
         dialect = csv.Sniffer().sniff(f.read(10240))
         # Check the delimiter.
-        if delimiter is None:
+        if verbose:
             print("Auto-detected delimiter: '{}'".format( \
                 dialect.delimiter))
-        else:
-            if dialect.delimiter != delimiter:
-                print("Detected delimiter '{}'".format(dialect.delimiter) + \
-                    ", but overwriting with specified delimiter" + \
-                    " '{}'".format(delimiter))
-            else:
-                print("Using delimiter: '{}'".format( \
-                    dialect.delimiter))
                 
         # Set the reading position back to the start of the file.
         f.seek(0)
@@ -59,10 +47,16 @@ def read_file(file_path, trial_start_zone, custom_fields=None, delimiter=None):
         len_header = len(header)
         
         # Get the default fields.
-        iparticipant = header.index("Participant Private ID")
+        ipublic = header.index("Participant Public ID")
+        iprivate = header.index("Participant Private ID")
+        if use_public_id:
+            iparticipant = ipublic
+        else:
+            iparticipant = iprivate
         iresolution = header.index("Participant Monitor Size")
         iviewport = header.index("Participant Viewport Size")
-        izone = header.index("Zone Name")
+        itrial = header.index("Trial Number")
+        izone = header.index("Zone Type")
         iresp = header.index("Response")
         itime = header.index("Reaction Onset")
         
@@ -86,6 +80,10 @@ def read_file(file_path, trial_start_zone, custom_fields=None, delimiter=None):
             
             # Check if the line is the expected length.
             if len(line) != len_header:
+                if verbose:
+                    print("Row {} is of the wrong number ".format(i+1) + \
+                        "of cells (expected {}). ".format(len_header) + \
+                        "Its content is:\n{}".format(line))
                 continue
             
             # Check the participant number.
@@ -96,40 +94,59 @@ def read_file(file_path, trial_start_zone, custom_fields=None, delimiter=None):
                     "trials":[], \
                     "resolution":line[iresolution], \
                     "viewport":line[iviewport], \
+                    "public_id":line[ipublic], \
+                    "private_id":line[iprivate], \
                     }
-
-            # Update the trial.
-            if line[izone] == trial_start_zone:
-                # Create new trial.
-                data[current_participant]["trials"].append({ \
-                    "msg":[], \
-                    "time":[], \
-                    "x":[], \
-                    "y":[], \
-                    })
-                # Add the custom field data.
-                for field in custom_fields:
-                    data[current_participant]["trials"][-1]["msg"].append( \
-                        [field, line[ifields[field]]])
             
-            # Only record if a trial has started.
-            if len(data[current_participant]["trials"]) > 0:
+            # Get the trial number.
+            trial_nr = line[itrial]
+            
+            # Check if this is a MouseView line, and if the reported file is
+            # local (we need) or online (we ignore). The zone type should be
+            # "mouse_view", but let's add in a view options in case this
+            # changes in the future.
+            if line[izone] in ["mouse_view", "mouseview", "MouseView"]:
 
-                # Add new coordinates to the time, x, and y fields.
-                if line[izone] == "coordinate":
-                    # Read the time.
-                    data[current_participant]["trials"][-1]["time"].append(\
-                        float(line[itime]))
-                    # Read and split the coordinates.
-                    x, y = line[iresp].split(" ")
-                    # Add it to the list
-                    data[current_participant]["trials"][-1]["x"].append(x)
-                    data[current_participant]["trials"][-1]["y"].append(y)
-                # Add any other data to the messages.
+                # Skip lines with a URL file path.
+                if line[iresp][:8] == "https://":
+                    continue
+
+                # Get the file name, and construct the path to the local file.
+                fname = line[iresp]
+                file_path = os.path.join(trial_folder_path, fname)
+                # Load the data, if it exists.
+                if os.path.isfile(file_path):
+                    participant, viewport, trial = \
+                        read_single_trial_file(file_path, verbose=verbose)
+                # Use an empty trial if the file cannot be found.
                 else:
-                    data[current_participant]["trials"][-1]["msg"].append( \
-                        [line[izone], line[iresp]])
-        
+                    if verbose:
+                        print("Participant {} ".format(current_participant) + \
+                            "trial {} could not be found.".format(trial_nr) + \
+                            "\nMissing file: {}".format(file_path))
+                    trial = { \
+                        "msg":[], \
+                        "time":numpy.array([], dtype=numpy.float32), \
+                        "x":numpy.array([], dtype=numpy.float32), \
+                        "y":numpy.array([], dtype=numpy.float32), \
+                        }
+                # Add the custom fields.
+                for field in custom_fields:
+                    trial["msg"].insert(0, [field, line[ifields[field]]])
+                
+                # Double-check that this is the same participant. (If not:
+                # something went wrong in the Gorilla file, as the file name
+                # comes from there! Or someone messed with either the OG file
+                # or the trial file names...)
+                if participant != line[iprivate]:
+                    raise Exception("File {} is listed for ".format(fname) + \
+                        "participant {}, ".format(current_participant) + \
+                        "but the file itself reports to be from " + \
+                        "participant {}".format(participant))
+                
+                # Add the trial to the current participant.
+                data[current_participant]["trials"].append(trial)
+
     # Convert all data lists to NumPy arrays.
     for participant in data.keys():
         for i in range(len(data[participant]["trials"])):
@@ -139,4 +156,166 @@ def read_file(file_path, trial_start_zone, custom_fields=None, delimiter=None):
     
     return data
 
+    
+def read_folder(folder_path, verbose=False):
+    
+    # Check whether the folder exists.
+    if not os.path.isdir(folder_path):
+        raise Exception("Folder not found at specified path: {}".format( \
+            folder_path))
+    
+    # Create an empty structure for the data. This will hold all participants
+    # (each with their own key), and under each participant it will have keys
+    # for trials (list of all trials, each is their own dict structure), 
+    # viewport (automatically read from file), resolution (not actually in the 
+    # files, so simply a copy of viewport; it's here for backwards
+    # compatibility), and participant_id (copy of the initial key name, but
+    # included in case the key name is changed).
+    data = {}
+    
+    # Read all the files in the folder.
+    all_files = os.listdir(folder_path)
+    # Sort alphabetically (this makes .
+    all_files.sort()
+    # Count all the files.
+    n_files = len(all_files)
+    
+    # Loop through all files.
+    for fi, fname in enumerate(all_files):
+    
+        # Construct the file path.
+        file_path = os.path.join(folder_path, fname)
+        # Split name and extension from the full name.
+        name, ext = os.path.splitext(fname)
 
+        # Skip lock files.
+        if name[:2] == "~$":
+            continue
+        # Check whether the file is of the right type.
+        ext = ext.lower()
+        if ext not in [".xls", ".xlsx"]:
+            if verbose:
+                print("File {} was not recognised as an MS ".format(fname) + \
+                    "Excel file. Its extension is '{}', but ".format(ext) + \
+                    "'.xls' or '.xlsx' was expected.")
+            continue
+        
+        if verbose:
+            print("Reading file '{}' ({}/{})".format(fname, fi+1, n_files))
+        
+        # Load the data.
+        current_participant, viewport, trial = read_single_trial_file( \
+            file_path, verbose=verbose)
+        
+        # Create a new entry for the current participant, if the ID is new.
+        if current_participant not in data.keys():
+            # Add a new entry for the current participant.
+            data[current_participant] = { \
+                "trials":[], \
+                "resolution":None, \
+                "viewport":None, \
+                }
+        
+        # Store the viewport data (copy to resolution, as we do not know what
+        # the resolution is from just the single-trial data).
+        if data[current_participant]["viewport"] is None:
+            data[current_participant]["viewport"] = copy.deepcopy(viewport)
+            data[current_participant]["resolution"] = copy.deepcopy(viewport)
+        
+        # Store the trial data.
+        data[current_participant]["trials"].append(copy.deepcopy(trial))
+
+    # Convert all data lists to NumPy arrays.
+    for participant in data.keys():
+        for i in range(len(data[participant]["trials"])):
+            for key in ["time", "x", "y"]:
+                data[participant]["trials"][i][key] = numpy.array( \
+                    data[participant]["trials"][i][key], dtype=numpy.float32)
+    
+    return data
+
+
+def read_single_trial_file(file_path, custom_fields=None, verbose=False):
+    
+    # Open the file using openpyxl.
+    workbook = openpyxl.load_workbook(file_path, read_only=True, \
+        data_only=True)
+    # Get the active sheet (this is the first one, which should be the
+    # only one in the Gorilla output).
+    sheet = workbook.active
+    # Get the first row as the header.
+    first_row = sheet.rows.next()
+    header = [cell.value for cell in first_row]
+    len_header = len(header)
+    
+    # Get the index numbers for important rows. (We do this once, to avoid
+    # having to call "index" on each iteration.)
+    iparticipant = header.index("participant_id")
+    itype = header.index("type")
+    izone = header.index("zone_name")
+    ihor = header.index("zone_x")
+    iver = header.index("zone_y")
+    iwidth = header.index("zone_width")
+    iheight = header.index("zone_height")
+    itime = header.index("time_stamp")
+    ix = header.index("x")
+    iy = header.index("y")
+    
+    # Store the values for the current trial in this trial dict.
+    trial = { \
+        "msg":[], \
+        "time":[], \
+        "x":[], \
+        "y":[], \
+        }
+    
+    # Loop through all the rows, and parse them as necessary.
+    current_participant = None
+    viewport = None
+    for i, row in enumerate(sheet.rows):
+
+        # Check if the line is the expected length.
+        if len(row) != len_header:
+            if verbose:
+                print("\tRow {} is of the wrong number ".format(i+1) + \
+                    "of cells (expected {}). ".format(len_header) + \
+                    "Its content is:\n\t{}".format(\
+                    str([cell.value for cell in row])))
+            continue
+        
+        # Get the participant ID.
+        if current_participant is None:
+            # Get the current participant ID.
+            current_participant = row[iparticipant].value
+        
+        # First, check if this row is for MouseView coordinates.
+        if row[itype].value == "mouseview":
+            
+            # Parse the MouseView coordinates.
+            trial["time"].append(float(row[itime].value))
+            trial["x"].append(float(row[ix].value))
+            trial["y"].append(float(row[iy].value))
+        
+        # Process rows that are NOT MouseView coordinates.
+        else:
+            # Add the row as a message.
+            t = float(row[itime])
+            msg = \
+                "type={};zone={};zone_x={};zone_y={};zone_w={};zone_h={}" \
+                .format(row[itype].value, row[izone].value, \
+                row[ihor].value, row[iver].value, row[iwidth].value, \
+                row[iheight].value)
+            trial["msg"].append((t, msg))
+            
+            # If this row has zone name "screen", use it to get the screen
+            # width and height. This is the viewport, but because there is
+            # no additional information on the resolution, we will also copy
+            # copy the viewrect into the "resolution" field. This is for 
+            # backwards compatibility.
+            if row[itype].value == "zone":
+                if row[izone].value == "screen":
+                    w = row[iwidth].value
+                    h = row[iheight].value
+                    viewport = "{}x{}".format(w, h)
+    
+    return current_participant, viewport, trial
